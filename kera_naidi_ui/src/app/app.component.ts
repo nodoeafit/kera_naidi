1607d49795153c9e9d3c30c7aa7cb619a529b7b6
Luis Robles <dracvs@gmail.com>
2025-02-20 13:53:28 -0500

    Angular App Componet.

--- kera_naidi_ui/src/app/app.component.ts
+++ kera_naidi_ui/src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { HealthService } from './services/health.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'kera_naidi_ui';
+
+  /**
+   *
+   */
+  constructor(private healthService : HealthService) {
+    
+  }
+
+
+  getHealth(){
+    this.healthService.getHealth().subscribe({
+        next: ((data: any)=>{
+            // Haz algo con lo que viene llegando
+        })
+    }); 
+  }
 }
---
+++ kera_naidi_ui/src/app/pages/healthcheck/healthcheck.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-healthcheck',
+  standalone: true,
+  imports: [],
+  templateUrl: './healthcheck.component.html',
+  styleUrl: './healthcheck.component.css'
+})
+export class HealthcheckComponent {
+
+}
